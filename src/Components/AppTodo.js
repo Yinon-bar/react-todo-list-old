6dd21e4abeb182a77b7e0e1d1b4d0d5248ec16e7
Yinon Bar <yinonbar1988@gmail.com>
2022-11-04 13:11:06 +0200

    clean-slate

--- src/Components/AppTodo.js
+++ src/Components/AppTodo.js
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import TaskInput from "./TaskInput";
 import TaskList from "./TaskList";
 
 function AppTodo() {
+  let [tasksArr, setTaskArr] = useState([]);
+
+  const addTask = (itemTask) => {
+    setTaskArr([...tasksArr, itemTask]);
+  };
+
   return (
     <div className="container">
       <h1 className="display-4 text-center">Todo List- Yinon - Bar</h1>
       <div className="col-lg-6 p-3 mx-auto">
-        <TaskInput />
+        <TaskInput addData={addTask} />
         <TaskList />
       </div>
     </div>
--- src/Components/TaskInput.js
+++ src/Components/TaskInput.js
@@ -1,13 +1,38 @@
-function TaskInput() {
+import { useRef } from "react";
+
+function TaskInput(props) {
+  const nameRef = useRef();
+  const timeRef = useRef();
+
+  const onAddTask = () => {
+    console.log(nameRef.current.value);
+    console.log(timeRef.current.value);
+
+    const todoItem = {
+      name: nameRef.current.value,
+      time: timeRef.current.value,
+      id: Date.now(),
+    };
+    props.addData(todoItem);
+    console.log(todoItem);
+  };
+
   return (
     <div>
       TaskInput
       <h2>Enter Task Name: </h2>
-      <input type="text" className="form-control" />
+      <input ref={nameRef} type="text" className="form-control" />
       <h2>Enter Task Time: </h2>
-      <input type="time" defaultValue="09:30" className="form-control" />
+      <input
+        ref={timeRef}
+        type="time"
+        defaultValue="09:30"
+        className="form-control"
+      />
       <div className="btns text-center my-4">
-        <button className="btn btn-success me-3">Add</button>
+        <button onClick={onAddTask} className="btn btn-success me-3">
+          Add
+        </button>
         <button className="btn btn-danger">Rests Tasks</button>
       </div>
     </div>
