6ab852ccf273f66898b65855a580afacc28c19c8
Yinon Bar <yinonbar1988@gmail.com>
2022-11-04 12:22:00 +0200

    clean-slate

--- src/Components/AppTodo.js
+++ src/Components/AppTodo.js
@@ -4,9 +4,11 @@ import TaskList from "./TaskList";
 function AppTodo() {
   return (
     <div className="container">
-      <h1>App todo</h1>
-      <TaskInput />
-      <TaskList />
+      <h1 className="display-4 text-center">Todo List- Yinon - Bar</h1>
+      <div className="col-lg-6 p-3 mx-auto">
+        <TaskInput />
+        <TaskList />
+      </div>
     </div>
   );
 }
--- src/Components/TaskInput.js
+++ src/Components/TaskInput.js
@@ -1,5 +1,11 @@
 function TaskInput() {
-  return <div>TaskInput</div>;
+  return (
+    <div>
+      TaskInput
+      <h2>Enter a Task: </h2>
+      <input type="text" className="form-control" />
+    </div>
+  );
 }
 
 export default TaskInput;
